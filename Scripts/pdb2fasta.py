9dea1a58d1e99b525bdc713a4bb80a040d5ba0f4
Bhagesh Hunakunti <hunakunti.basavraj@learner.manipal.edu>
2021-04-24 13:49:14 +0530

    pdb2fasta added

--- Scripts/pdb2fasta.py
+++ Scripts/pdb2fasta.py
@@ -1,7 +1,38 @@
+import warnings
+import argparse
+from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.Polypeptide import PPBuilder
 
-ppb=PPBuilder()
-protseq = ''
-for pp in ppb.build_peptides(structure):
-    protseq += str(pp.get_sequence())
-print(protseq)
+#To ignore warnings due to PDB file
+warnings.filterwarnings('ignore')
+
+#Argparse arguments
+parser = argparse.ArgumentParser(description='\n PDB to Fasta converter.')
+parser.add_argument("-i","--input", help = "input filepath/filename of PDB file example: Data/inputfile.pdb")
+parser.add_argument("-o","--output", help = "output filepath/filename of fasta file example: Data/outputfile.fasta")
+
+args = parser.parse_args()
+
+#conditions for input & output files
+if not args.input:
+    print("\n Please enter input file name using -i or --input")
+elif not args.output:
+    print("\n Please enter Output file name using -o or --ouput")
+else:
+    inputpath = args.input
+    outputpath = args.output
+
+    #PDB parser 
+    parser = PDBParser()
+    structure = parser.get_structure("",inputpath)
+
+    #for loop to join multiple chains into a single chain
+    ppb=PPBuilder()
+    protseq = ''
+    for pp in ppb.build_peptides(structure):
+        protseq += str(pp.get_sequence())
+
+    #writing to a file
+    with open(outputpath,'w') as f:
+        f.write(">"+outputpath.replace("/","").replace(".","")+"\n"+protseq)
+        print("\n Fasta file has been created in the folder: "+outputpath)
